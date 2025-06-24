dcb43df0977a178f9f59a8e56f046b1a99f2f148
Ansari-Mohd-Saad <157679873+MohdSaad7766@users.noreply.github.com>
2025-06-24 11:58:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+const input = document.getElementById("fname");
+
+input.addEventListener("blur", function () {
+  input.value = input.value.toUpperCase();
+});
+
+// Do not change the code below
+
+const s = prompt("Enter String:");
+alert(firstWord(s));
